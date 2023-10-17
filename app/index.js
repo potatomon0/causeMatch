c0d7bc0a091f31109ca97cfbb4b921cc6e30171a
Hu <huang.h0709@gmail.com>
2023-10-17 18:19:51 -0400

    fixed bottom navigator

--- app/index.js
+++ app/index.js
@@ -1,25 +1,26 @@
-import { View, Text, ScrollView, SafeAreaView } from 'react-native'
-import {Stack} from 'expo-router'
+import { View, Text, ScrollView, SafeAreaView, StyleSheet } from 'react-native'
+import React from 'react';
+import { Stack } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../components/Home/Home'
 import Message from '../components/Message/Message'
 import Profile from '../components/Profile/Profile'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
 const Main = () => {
     const Tab = createBottomTabNavigator()
+    const styles = StyleSheet.create({
+        safeAreaView: {
+
+        }
+    })
     return (
-        <SafeAreaView>
             <Tab.Navigator>
                 <Tab.Screen name="Home" component={Home} />
                 <Tab.Screen name="Message" component={Message} />
                 <Tab.Screen name="Profile" component={Profile} />
-            </Tab.Navigator> 
-            
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+            </Tab.Navigator>
     )
 }
 
